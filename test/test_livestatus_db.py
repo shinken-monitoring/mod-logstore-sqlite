c3fdf9895479854563ffa823195f2ffb81fac257
Sébastein Coavoux <s.coavoux@free.fr>
2014-01-21 21:33:53 -0500

    Fix test. See naparuba@a790fb22231d653ff3f1cda76f0efb680655a257

--- test/test_livestatus_db.py
+++ test/test_livestatus_db.py
@@ -106,7 +106,7 @@ class TestConfig(ShinkenModulesTest):
 
 class TestConfigSmall(TestConfig):
     def setUp(self):
-        self.setup_with_file('etc/nagios_1r_1h_1s.cfg')
+        self.setup_with_file('etc/shinken_1r_1h_1s.cfg')
         Comment.id = 1
         self.testid = str(os.getpid() + random.randint(1, 1000))
         self.init_livestatus()
@@ -556,7 +556,7 @@ class TestConfigBig(TestConfig):
 
     def setUp(self):
         start_setUp = time.time()
-        self.setup_with_file('etc/nagios_5r_100h_2000s.cfg')
+        self.setup_with_file('etc/shinken_5r_100h_2000s.cfg')
         Comment.id = 1
         self.testid = str(os.getpid() + random.randint(1, 1000))
         self.init_livestatus()
@@ -785,7 +785,7 @@ class TestConfigNoLogstore(TestConfig):
 
     def setUp(self):
         start_setUp = time.time()
-        self.setup_with_file('etc/nagios_1r_1h_1s.cfg')
+        self.setup_with_file('etc/shinken_1r_1h_1s.cfg')
         Comment.id = 1
         self.testid = str(os.getpid() + random.randint(1, 1000))
         self.init_livestatus()
